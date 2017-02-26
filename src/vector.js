f42e2adc0f9e1a9de9142c488e7fa1f34f7a9c34
Megh Parikh <meghprkh@gmail.com>
2017-02-27 04:32:46 +0530

    Add vector library

---
+++ src/vector.js
@@ -0,0 +1,36 @@
+function dot([x, y, z], [p, q, r]) {
+  return x*p + y*q + z*r
+}
+
+function cross([ux, uy, uz], [vx, vy, vz]) {
+  var x = uy*vz - uz*vy;
+  var y = uz*vx - ux*vz;
+  var z = ux*vy - uy*vx;
+  return [x, y, z];
+}
+
+function add([x, y, z], [p, q, r]) {
+  return [x + p, y + q, z + r]
+}
+
+function subtract([x, y, z], [p, q, r]) {
+  return [x - p, y - q, z - r]
+}
+
+function abs([x, y, z]) {
+  return Math.sqrt(x*x + y*y + z*z)
+}
+
+function normalize([x, y, z]) {
+  var t = abs([x, y, z])
+  return [x/t, y/t, z/t]
+}
+
+module.exports = {
+  dot,
+  cross,
+  add,
+  subtract,
+  abs,
+  normalize,
+}
